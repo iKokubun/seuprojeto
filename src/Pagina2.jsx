35448c5d2fcefa49d327c2409b804e7c6074f6d8
iKokubun <iankokubun@gmail.com>
2026-02-13 16:41:27 -0300

    feat: adicionar Pagina2 em /pagina2 e configurar rotas

---
+++ src/Pagina2.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Pagina2() {
+    return (
+        <div>
+            <h1>Título da Página 2</h1>
+            <h2>Este é o subtítulo da página 2</h2>
+        </div>
+    );
+}
+
+export default Pagina2;
